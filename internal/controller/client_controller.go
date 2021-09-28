949fd25d43cbf51305caacaab90052e48d45d107
rustingoff <nailunho@gmail.com>
2021-09-28 21:47:16 +0300

    master -- added models

--- internal/controller/client_controller.go
+++ internal/controller/client_controller.go
@@ -1,1 +1,18 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+type ClientController interface {
+	CreateClient(c *gin.Context)
+}
+
+type clientController struct {
+}
+
+func GetClientController() ClientController {
+	return &clientController{}
+}
+
+func (cc *clientController) CreateClient(c *gin.Context) {
+
+}
